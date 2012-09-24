797cccd7a4102436117955e98d4b35ec3010a632
Ross Hettel <rosshettel@gmail.com>
2012-09-24 14:21:12 -0500

    initial commit

---
+++ 24time.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#set the directory to the folder the script resides in
+DIRECTORY=$(cd `dirname $0` && pwd)
+
+# get the hour number, convert to base 10
+let hourNum=10#`date "+%H"`
+
+#echo $DIRECTORY/$hourNum
+
+afplay $DIRECTORY/$hourNum.mp3
